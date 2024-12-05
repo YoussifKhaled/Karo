2b8a257687d24acbcb088e729b6fffa97d1daee1
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-05 19:52:09 +0200

    Initialize frontend and backend

---
+++ backend/src/main/java/com/example/karo/KaroApplication.java
@@ -0,0 +1,13 @@
+package com.example.karo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaroApplication.class, args);
+	}
+
+}
