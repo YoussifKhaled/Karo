63bd0d55869d7cc14673cad02f4e8e20c62d3585
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-12-25 23:37:10 +0200

    Writing SQL queries

--- backend/src/main/java/com/example/karo/repositories/ParkingSpotRepository.java
+++ backend/src/main/java/com/example/karo/repositories/ParkingSpotRepository.java
@@ -9,6 +9,44 @@ import java.util.List;
 
 @Repository
 public class ParkingSpotRepository {
+    private static final String SQL_INSERT_SPOT = """
+            INSERT INTO parking_spot
+            	(spot_id, lot_id, type, sensor_id, spot_status, sensor_status, price)
+            VALUES
+            	(?, ?, ?, ?, ?, ?, ?);
+            """;
+    private static final String SQL_FIND_SPOT_BY_ID = """
+            SELECT * FROM parking_spot
+            WHERE spot_id = ?;
+            """;
+    private static final String SQL_FIND_SPOTS_BY_LOT_ID = """
+            SELECT * FROM parking_spot
+            WHERE lot_id = ?;
+            """;
+    private static final String SQL_UPDATE_SPOT_TYPE = """
+            UPDATE parking_spot
+            SET type = ?
+            WHERE spot_id = ?;
+            """;
+    private static final String SQL_UPDATE_SPOT_STATUS = """
+            UPDATE parking_spot
+            SET spot_status = ?
+            WHERE spot_id = ?;
+            """;
+    private static final String SQL_UPDATE_SENSOR_STATUS = """
+            UPDATE parking_spot
+            SET sensor_status = ?
+            WHERE spot_id = ?;
+            """;
+    private static final String SQL_UPDATE_SPOT_PRICE = """
+            UPDATE parking_spot
+            SET price = ?
+            WHERE spot_id = ?;
+            """;
+    private static final String SQL_DELETE_SPOT_BY_ID = """
+            DELETE FROM parking_spot
+            WHERE spot_id = ?;
+            """;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
