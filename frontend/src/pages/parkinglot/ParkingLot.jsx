9f5c89f27c9becec0588d4957343cd84c386e7a9
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-25 21:01:00 +0200

    feat - implement parking lot layout and display parking spots

--- frontend/src/pages/parkinglot/ParkingLot.jsx
+++ frontend/src/pages/parkinglot/ParkingLot.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import './ParkingLot.css';
 import { useParams } from 'react-router-dom';
 import Header from '../../components/header/Header';
+import EvStationIcon from '@mui/icons-material/EvStation';
+import AccessibleIcon from '@mui/icons-material/Accessible';
+
+const parkingSpots = [
+    { id: 1, status: 'open', type: 'regular' },
+    { id: 2, status: 'open', type: 'disabled' },
+    { id: 3, status: 'open', type: 'regular' },
+    { id: 4, status: 'open', type: 'regular' },
+    { id: 5, status: 'open', type: 'ev' },
+    { id: 6, status: 'open', type: 'regular' },
+    { id: 7, status: 'open', type: 'regular' },
+    { id: 8, status: 'reserved', type: 'regular' },
+    { id: 9, status: 'open', type: 'regular' },
+    { id: 10, status: 'open', type: 'disabled' },
+    { id: 11, status: 'reserved', type: 'regular' },
+    { id: 12, status: 'open', type: 'ev' },
+    { id: 13, status: 'open', type: 'regular' },
+    { id: 14, status: 'reserved', type: 'disabled' },
+    { id: 15, status: 'open', type: 'regular' }
+]
 
 function ParkingLot() {
 
     const { lotId } = useParams();
 
     return (
-        <div>
+        <div className='parking-lot'>
             <Header title={`Parking Lot ${lotId}`}/>
+            <ul className='parking-spots'>
+                {parkingSpots.map(parkingSpot => (
+                    <li key={parkingSpot.id} className={`parking-spot`}>
+                        <span className='spot-title'>Spot {parkingSpot.id}</span>
+                        {parkingSpot.type === 'ev' && <EvStationIcon style={{ color: 'green' }} className="spot-type" />}
+                        {parkingSpot.type === 'disabled' && <AccessibleIcon className="spot-type"/>}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
