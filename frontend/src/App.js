c7c82c64d16dba6f94112e22a5f577ecdd3087bb
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-25 16:19:33 +0200

    feat - add ParkingLot route to application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Home from './pages/home/Home';
+import ParkingLot from './pages/parkinglot/ParkingLot';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<Signup />} /> 
             <Route path="/home" element={<Home />} />
+            <Route path="/parking-lot/:lotId" element={<ParkingLot />} />
           </Routes> 
         </main>
       </Router>
