8c2f90c925766801d5f9da92e746ab678589c299
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-25 16:20:57 +0200

    feat - implement Home page layout with Header and MapComponent

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
 import './Home.css';
 import React from 'react';
+import MapComponent from '../../components/map/Map';
+import Header from '../../components/header/Header';
 
 function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
+        <Header title="Karo" />
+        <div className="map-container">
+            <MapComponent />
+        </div>
     </div>
   );
 }
