727a1283654e0a183c5c940a7e234c4175068fd6
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-26 18:14:48 +0200

    feat: add reservation component and styling for parking lot reservations

--- frontend/src/pages/parkinglot/ParkingLot.jsx
+++ frontend/src/pages/parkinglot/ParkingLot.jsx
@@ -1,8 +1,10 @@
 import './ParkingLot.css';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../../components/header/Header';
 import EvStationIcon from '@mui/icons-material/EvStation';
 import AccessibleIcon from '@mui/icons-material/Accessible';
+import Reservation from './../../components/reservation/Reservation';
 
 const parkingSpots = [
     { id: 1, status: 'open', type: 'regular' },
@@ -25,13 +27,17 @@ const parkingSpots = [
 function ParkingLot() {
 
     const { lotId } = useParams();
+    const [reservingSpot, setReservingSpot] = useState(null);
 
     return (
         <div className='parking-lot'>
             <Header title={`Parking Lot ${lotId}`}/>
+            {reservingSpot &&
+                <Reservation spot={reservingSpot} onClose={() => setReservingSpot(null)} />
+            }
             <ul className='parking-spots'>
                 {parkingSpots.map(parkingSpot => (
-                    <li key={parkingSpot.id} className={`parking-spot`}>
+                    <li key={parkingSpot.id} className={`parking-spot`} onClick={() => setReservingSpot(parkingSpot)}>
                         <span className='spot-title'>Spot {parkingSpot.id}</span>
                         {parkingSpot.type === 'ev' && <EvStationIcon style={{ color: 'green' }} className="spot-type" />}
                         {parkingSpot.type === 'disabled' && <AccessibleIcon className="spot-type"/>}
