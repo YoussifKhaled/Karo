7e3d0393c94ada3b1b5d3a011bc96da8908f54d8
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-27 20:01:36 +0200

    feat - Enhance Home page layout with admin lists for top drivers, parking lots, and system managers

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,18 +1,50 @@
 import './Home.css';
-import React from 'react';
+import { useState } from 'react';
 import MapComponent from '../../components/map/Map';
 import Header from '../../components/header/Header';
+
 
 // mode: Driver:0 or Manager:1 or Admin:2
 const mode = 2;
 
 function Home() {
+
+  const [topDrivers, setTopDrivers] = useState([]);
+  const [topParkingLots, setTopParkingLots] = useState([]);
+  const [systemManagers, setSystemManagers] = useState([]);
+
   return (
     <div className="Home">
         <Header title="Karo" mode={mode} />
-        <div className="map-container">
-            <MapComponent mode={mode} />
-        </div>
+        {
+          mode === 0 &&
+          <div className="map-container">
+              <MapComponent mode={mode} />
+          </div>
+        }
+        {
+          mode === 1 &&
+          <div className="map-container">
+              <MapComponent mode={mode} />
+          </div>
+        }
+        {
+          mode === 2 &&
+          <div className="lists-container">
+              <ul className="admin-list">
+                <div className="list-title">Top Drivers</div>
+              
+              </ul>
+              <ul className="admin-list">
+                <div className="list-title">Top Parking Lots</div>
+
+              </ul>
+              <ul className="admin-list">
+                <div className="list-title">System Managers</div>
+
+              </ul>
+          </div>
+        }
     </div>
   );
 }
