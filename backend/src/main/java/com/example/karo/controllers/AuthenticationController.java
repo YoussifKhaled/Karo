2b5c20c289281e08e5646e66bb98198aec146f5d
Mohamed Ashraf <mohamed.code.13@gmail.com>
2024-12-27 20:00:42 +0200

    Creating deleting endpoints

--- backend/src/main/java/com/example/karo/controllers/AuthenticationController.java
+++ backend/src/main/java/com/example/karo/controllers/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.example.karo.controllers;
 
 import com.example.karo.models.dtos.RegisterUserDTO;
 import com.example.karo.services.AuthenticationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,5 +23,14 @@ public class AuthenticationController {
     public ResponseEntity<String> login(@RequestParam String email,
                                         @RequestParam String password) {
         return authenticationService.login(email, password);
+    }
+
+    @DeleteMapping("/delete-user/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long userId) {
+        try {
+            return new ResponseEntity<>(authenticationService.deleteUserById(userId), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
     }
 }
