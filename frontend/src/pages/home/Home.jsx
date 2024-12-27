2b991f3a53243e0c13d267013c1f6dcf3ed9b492
Youssif Khaled <116981986+YoussifKhaled@users.noreply.github.com>
2024-12-27 21:36:43 +0200

    Frontend admin (#22)
    * feat - Implement manager creation with API integration and error handling
    
    * feat - Enhance Home page layout with admin lists for top drivers, parking lots, and system managers
    
    * fix - Change JOIN to LEFT JOIN in AdminRepository to include drivers with no reservations
    
    * refactor - Remove React.StrictMode from the root rendering in index.js
    
    * feat - Implement admin functionality to fetch and delete top drivers, parking lots, and system managers

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,18 +1,171 @@
 import './Home.css';
-import React from 'react';
+import { useState,useEffect,useCallback } from 'react';
 import MapComponent from '../../components/map/Map';
 import Header from '../../components/header/Header';
 
+
 // mode: Driver:0 or Manager:1 or Admin:2
-const mode = 2;
+const mode = 0;
 
 function Home() {
+
+  const token = localStorage.getItem('token');
+
+  const [topDrivers, setTopDrivers] = useState([]);
+  const [topParkingLots, setTopParkingLots] = useState([]);
+  const [systemManagers, setSystemManagers] = useState([]);
+
+
+  const fetchAllData = useCallback(async () => {
+    try {
+      const driverResponse = await fetch('http://localhost:8080/admin/top-users', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const driversData = await driverResponse.json();
+      setTopDrivers(driversData);
+
+      const lotsResponse = await fetch('http://localhost:8080/admin/top-lots', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const lotsData = await lotsResponse.json();
+      setTopParkingLots(lotsData);
+  
+      const managerResponse = await fetch('http://localhost:8080/admin/all-managers', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const managerData = await managerResponse.json();
+      setSystemManagers(managerData);
+    }catch (error) {
+      console.log(error);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    // fetch top drivers
+    fetchAllData();
+  }, [fetchAllData]);
+
+  const deleteDriver = async (userId) => {
+    const response = await fetch(`http://localhost:8080/delete-user/${userId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    fetchAllData();
+  }
+
+  const deleteParkingLot = async (lotId) => {
+    const response = await fetch(`http://localhost:8080/lot/delete/${lotId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    fetchAllData();
+  }
+
+  const deleteSystemManager = async (userId) => {
+    const response = await fetch(`http://localhost:8080/delete-user/${userId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    fetchAllData();
+  }
+
   return (
     <div className="Home">
         <Header title="Karo" mode={mode} />
-        <div className="map-container">
-            <MapComponent mode={mode} />
-        </div>
+        {
+          mode === 0 &&
+          <div className="map-container">
+              <MapComponent mode={mode} />
+          </div>
+        }
+        {
+          mode === 1 &&
+          <div className="map-container">
+              <MapComponent mode={mode} />
+          </div>
+        }
+        {
+          mode === 2 &&
+          <div className="lists-container">
+              <ul className="admin-list">
+                <div className="list-title">Top Drivers</div>
+                {
+                  topDrivers.map(driver => (
+                    <li key={driver.userId} className="driver-list-item">
+                      <span>{driver.name}</span>
+                      <span>{driver.reservationCount}</span>
+                      <span
+                        className="delete-icon"
+                        onClick={() => deleteDriver(driver.userId)}
+                      >
+                        🗑
+                      </span>
+                    </li>
+                  ))
+                }
+              </ul>
+              <ul className="admin-list">
+                <div className="list-title">Top Parking Lots</div>
+                {
+                  topParkingLots.map(lot => (
+                    <li key={lot.lotId} className="lot-list-item">
+                      <span>lot {lot.lotId}</span>
+                      <span>{lot.revenue}</span>
+                      <span 
+                        className="delete-icon"
+                        onClick={() => deleteParkingLot(lot.lotId)}
+                      >
+                        🗑
+                      </span>
+                    </li>
+                  ))
+                }
+              </ul>
+              <ul className="admin-list">
+                <div className="list-title">System Managers</div>
+                {
+                  systemManagers.map(manager => (
+                    <li key={manager.userId} className="manager-list-item">
+                      <span>{manager.name}</span>
+                      <span 
+                        className="delete-icon"
+                        onClick={() => deleteSystemManager(manager.userId)}
+                      >
+                        🗑
+                      </span>
+                    </li>
+                  ))
+                }
+              </ul>
+          </div>
+        }
     </div>
   );
 }
