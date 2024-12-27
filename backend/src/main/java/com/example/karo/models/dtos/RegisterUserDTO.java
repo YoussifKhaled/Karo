e0ab054ee7a096eae4d870c7ad06d3643e349799
Isamel Mahmoud <120949930+esmailMahmouds@users.noreply.github.com>
2024-12-27 13:29:38 +0200

    Kar 70 authentication (#11)
    * Feat-User Authentication
    
    * edit Driver and User entities annotation
    
    * remove debug prints
    
    * Edit Authenication service
    
    * Edit User Entity
    
    * Add Driver Repository
    
    * Add userService
    
    * Add signUp and login Page
    
    * Handle error message of duplicate license plate
    
    * Add NoArgs,AllArgs constructors
    
    * Delte multiple security configuration

---
+++ backend/src/main/java/com/example/karo/models/dtos/RegisterUserDTO.java
@@ -0,0 +1,4 @@
+package com.example.karo.models.dtos;
+
+public record RegisterUserDTO(String name, String email, String password, String licensePlateNumber) {
+}
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,10 +1,105 @@
-
-
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {Link, useNavigate } from "react-router-dom";
+import Password from "../../components/password/Password";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    setErrorMessage("");
+
+    e.preventDefault();
+
+    try{
+      const response = await fetch(`http://localhost:8080/login?email=${email}&password=${password}`,{
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+      });
+
+      if(response.status === 200){
+          const token = await response.text();
+          localStorage.setItem("token", token);
+          console.log("Login successful");
+          navigate("/home");
+      }
+      else{
+          const errorMessage = await response.text();
+          console.log(errorMessage);
+          setErrorMessage("Invalid username or password");
+      }
+      }catch(error){
+          console.log("something went wrong");
+      }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Box bgcolor="#FBF6F6" height="100vh" 
+      sx={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+      }}
+    >
+      <Box
+        bgcolor="#DEDEDE"
+        width="300px"
+        p={3}
+        borderRadius={2}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          boxShadow: 3,
+        }}
+      >
+        <Stack
+          component="form"
+          onSubmit={handleLogin}
+          noValidate
+          sx={{ mt: 1 }}
+        >
+          <Typography component="h1" variant="h5" fontWeight="700">
+            Login
+          </Typography>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Password
+            value={password}
+            setValue={setPassword}
+          />
+          <Typography variant="subtitle2" color="red" height="10px">
+            {errorMessage}
+          </Typography>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+          <Typography variant="subtitle2" height="10px">
+            Don't have account? <Link to="/signup">SignUp</Link>
+          </Typography>
+        </Stack>
+      </Box>
+    </Box>
   );
 }
 
