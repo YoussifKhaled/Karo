362014b67f699fd63a63d919312b0599a8129e2c
Youssif Khaled <youssif13abdalaziz@gmail.com>
2024-12-27 17:58:59 +0200

    Add dropdown menu for account options in Header component

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -6,19 +7,40 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header ({title}) {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [dropdown, setDropdown] = useState(false);
+
+  const logOut = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  }
+
+  const goToWallet = () => {
+    navigate('/wallet');
+  }
 
   return (
-    <div className="header">
-        <HomeIcon className="home-icon" sx={{ fontSize: 30 }}
-        onClick={() => navigate('/home')}
-        />
-        <span className="karo">{title}</span>
-        <NotificationsIcon className="notification-icon" sx={{ fontSize: 30 }}
-        onClick={() => navigate('/notifications')}
-        />
-        <AccountCircleIcon className="account-icon" sx={{ fontSize: 30 }}/>
-    </div>
+    <>
+      <div className="header">
+          <HomeIcon className="home-icon" sx={{ fontSize: 30 }}
+          onClick={() => navigate('/home')}
+          />
+          <span className="karo">{title}</span>
+          <NotificationsIcon className="notification-icon" sx={{ fontSize: 30 }}
+          onClick={() => navigate('/notifications')}
+          />
+          <AccountCircleIcon
+            className="account-icon" sx={{ fontSize: 30 }}
+            onClick={() => setDropdown(!dropdown)}
+          />
+      </div>
+      {dropdown && (
+        <ul className="dropdown">
+            <li className="dropdown-element" onClick={goToWallet}>Wallet</li>
+            <li className="dropdown-element" onClick={logOut}>Logout</li>
+        </ul>
+      )}
+    </>
     );
 }
 
